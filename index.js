00303eb8cb5d9a5c7b8d102dbc4268a619c16e27
astreminix <66201263+astreminix@users.noreply.github.com>
2020-06-01 17:29:11 +1200

    Create index.js

---
+++ index.js
@@ -0,0 +1,280 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+
+client.on('ready', () => {
+    
+        console.log(`Logged in as ${client.user.username}, ready to get this bread.`);
+        client.user.setActivity('Testing stuff');
+    
+    });
+    
+client2.on('ready', () => {
+    
+        console.log(`Logged in as ${client2.user.username}, ready to get this bread.`);
+        client2.user.setActivity('Testing stuff');
+    
+    });
+    
+client3.on('ready', () => {
+    
+        console.log(`Logged in as ${client3.user.username}, ready to get this bread.`);
+        client3.user.setActivity('Testing stuff');
+    
+    });
+	
+client4.on('ready', () => {
+    
+        console.log(`Logged in as ${client4.user.username}, ready to get this bread.`);
+        client4.user.setActivity('Testing stuff');
+    
+    });
+		
+client5.on('ready', () => {
+    
+        console.log(`Logged in as ${client5.user.username}, ready to get this bread.`);
+        client5.user.setActivity('Testing stuff');
+    
+    });
+	
+client6.on('ready', () => {
+    
+        console.log(`Logged in as ${client6.user.username}, ready to get this bread.`);
+        client6.user.setActivity('Testing stuff');
+    
+    });
+	
+client7.on('ready', () => {
+    
+        console.log(`Logged in as ${client7.user.username}, ready to get this bread.`);
+        client7.user.setActivity('Testing stuff');
+    
+    });
+	
+client8.on('ready', () => {
+    
+        console.log(`Logged in as ${client8.user.username}, ready to get this bread.`);
+        client8.user.setActivity('Testing stuff');
+    
+    });
+	
+client9.on('ready', () => {
+    
+        console.log(`Logged in as ${client9.user.username}, ready to get this bread.`);
+        client9.user.setActivity('Testing stuff');
+    
+    });
+	
+client10.on('ready', () => {
+    
+        console.log(`Logged in as ${client10.user.username}, ready to get this bread.`);
+        client10.user.setActivity('Testing stuff');
+    
+    });
+	
+
+
+	function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
+client1.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start1`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client2.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start2`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client3.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start1`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client4.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start2`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client5.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start5`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client6.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start6`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client7.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start7`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client8.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start8`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client9.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start9`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+client10.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start10`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls buy laptop')
+await sleep(900000)
+}
+}
+});
+
+
+client.login(process.env.token)
+client2.login(process.env.token2)
+client3.login(process.env.token3)
+client4.login(process.env.token4)
+client5.login(process.env.token5)
+client6.login(process.env.token6)
+client7.login(process.env.token7)
+client8.login(process.env.token8)
+client9.login(process.env.token9)
+client10.login(process.env.token10)
