820495a59773ea309dae780c4a69f6ba822d2040
astreminix <66201263+astreminix@users.noreply.github.com>
2020-06-11 17:52:44 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,122 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const bot2 = new Discord.Client2();
+const bot3 = new Discord.Client3();
+const bot4 = new Discord.Client4();
+const bot5 = new Discord.Client5();
+const bot6 = new Discord.Client6();
+const bot7 = new Discord.Client7();
+const bot8 = new Discord.Client8();
+const bot9 = new Discord.Client9();
+const bot10 = new Discord.Client10();
+const bot11 = new Discord.Client11();
+const bot12 = new Discord.Client12();
+const bot13 = new Discord.Client13();
+const bot14 = new Discord.Client14();
+const bot15 = new Discord.Client15();
+const bot16 = new Discord.Client16();
+const bot17 = new Discord.Client17();
+const bot18 = new Discord.Client18();
+const bot19 = new Discord.Client19();
+const bot20 = new Discord.Client20();
 
-const client = new Discord.Client();
+bot.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
 
-function sleep(ms) {
-return new Promise(resolve => setTimeout(resolve, ms));
-}
+bot2.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
 
-client.on('ready', async () => {
+bot3.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
 
-var server = client.guilds.get("712773337864077382");
-var channel = server.channels.get("719307658271916085")
+bot4.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
 
-        console.log(`Logged in as ${client.user.username}, ready to auto bump.`);
-        for (var i = 0; i < 1000000; i++) {
-       channel.send('!d bump')
-        await sleep(7200000)
-}
- 
-    
-    });
+bot5.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
 
-client.login(process.env.token)
+bot6.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot7.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot8.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot9.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot10.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot11.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot12.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot13.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot14.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot15.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot16.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot17.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot18.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot19.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot20.on('guildMemberAdd', member => {
+   member.send("Come join our dank memer premium servers where we do massive daily giveaways and friendly heists! https://discord.gg/YSNGAKj https://discord.gg/3fpNYUA https://discord.gg/ZVFYD2K");
+});
+
+bot.login(process.env.token)
+bot2.login(process.env.token2)
+bot3.login(process.env.token3)
+bot4.login(process.env.token4)
+bot5.login(process.env.token5)
+bot6.login(process.env.token6)
+bot7.login(process.env.token7)
+bot8.login(process.env.token8)
+bot9.login(process.env.token9)
+bot10.login(process.env.token10)
+bot11.login(process.env.token11)
+bot12.login(process.env.token12)
+bot13.login(process.env.token13)
+bot14.login(process.env.token14)
+bot15.login(process.env.token15)
+bot16.login(process.env.token16)
+bot17.login(process.env.token17)
+bot18.login(process.env.token18)
+bot19.login(process.env.token19)
+bot20.login(process.env.token20)
