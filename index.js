23275f05064e73c39bffa4c560172a45f072db7c
astreminix <66201263+astreminix@users.noreply.github.com>
2020-06-15 17:42:24 +1200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const client7 = new Discord.Client();
 const client8 = new Discord.Client();
 const client9 = new Discord.Client();
 const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
 
 function sleep(ms) {
 return new Promise(resolve => setTimeout(resolve, ms));
@@ -22,24 +27,8 @@ var channel = server.channels.get("716227489860681743")
 
 console.log(`Logged in as ${client.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -52,24 +41,8 @@ var channel = server.channels.get("716227728743071824")
 
 console.log(`Logged in as ${client2.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -82,24 +55,8 @@ var channel = server.channels.get("716227761353523261")
 
 console.log(`Logged in as ${client3.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -112,24 +69,8 @@ var channel = server.channels.get("716227785382952981")
 
 console.log(`Logged in as ${client4.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -142,24 +83,8 @@ var channel = server.channels.get("716227855360458834")
 
 console.log(`Logged in as ${client5.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -172,24 +97,8 @@ var channel = server.channels.get("716227876902535198")
 
 console.log(`Logged in as ${client6.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -202,24 +111,8 @@ var channel = server.channels.get("716227900356952094")
 
 console.log(`Logged in as ${client7.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -232,24 +125,8 @@ var channel = server.channels.get("716227925724364831")
 
 console.log(`Logged in as ${client8.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -262,24 +139,8 @@ var channel = server.channels.get("716227947844861983")
 
 console.log(`Logged in as ${client9.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -292,24 +153,78 @@ var channel = server.channels.get("716227975518879805")
 
 console.log(`Logged in as ${client10.user.username}, ready to get this bread.`);
 for (var i = 0; i < 1000000; i++) {
-channel.send('pls search')
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls beg')
-await sleep(25100)
-channel.send('pls search');
-await sleep(1000)
-channel.send('dresser');
-await sleep(5000)
-channel.send('pls pm')
-await sleep(1000)
-channel.send('d');
-await sleep(8500)
-channel.send('pls beg')
-await sleep(31000)
-channel.send('pls use candy')
-await sleep(6500)
+channel.send('pls buy laptop')
+await sleep(900000)
+}
+
+
+    });
+
+client11.on('ready', async () => {
+
+var server = client11.guilds.get("716227489860681738");
+var channel = server.channels.get("716228032850821141")
+
+console.log(`Logged in as ${client11.user.username}, ready to get this bread.`);
+for (var i = 0; i < 1000000; i++) {
+channel.send('pls buy laptop')
+await sleep(900000)
+}
+
+
+    });
+
+client12.on('ready', async () => {
+
+var server = client12.guilds.get("716227489860681738");
+var channel = server.channels.get("716228076324782112")
+
+console.log(`Logged in as ${client12.user.username}, ready to get this bread.`);
+for (var i = 0; i < 1000000; i++) {
+channel.send('pls buy laptop')
+await sleep(900000)
+}
+
+
+    });
+
+client13.on('ready', async () => {
+
+var server = client13.guilds.get("716227489860681738");
+var channel = server.channels.get("716228134390857788")
+
+console.log(`Logged in as ${client13.user.username}, ready to get this bread.`);
+for (var i = 0; i < 1000000; i++) {
+channel.send('pls buy laptop')
+await sleep(900000)
+}
+
+
+    });
+
+client14.on('ready', async () => {
+
+var server = client14.guilds.get("716227489860681738");
+var channel = server.channels.get("716228161787920415")
+
+console.log(`Logged in as ${client14.user.username}, ready to get this bread.`);
+for (var i = 0; i < 1000000; i++) {
+channel.send('pls buy laptop')
+await sleep(900000)
+}
+
+
+    });
+
+client15.on('ready', async () => {
+
+var server = client15.guilds.get("716227489860681738");
+var channel = server.channels.get("716228182977544252")
+
+console.log(`Logged in as ${client15.user.username}, ready to get this bread.`);
+for (var i = 0; i < 1000000; i++) {
+channel.send('pls buy laptop')
+await sleep(900000)
 }
 
 
@@ -325,3 +240,8 @@ client7.login(process.env.token7)
 client8.login(process.env.token8)
 client9.login(process.env.token9)
 client10.login(process.env.token10)
+client11.login(process.env.token11)
+client12.login(process.env.token12)
+client13.login(process.env.token13)
+client14.login(process.env.token14)
+client15.login(process.env.token15)
